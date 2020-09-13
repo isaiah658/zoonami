1307d9e13e8f3626adf92c166ee291883b9c8373
isaiah658 <29903689+isaiah658@users.noreply.github.com>
2020-09-13 15:56:45 +0000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- Namespace
+zoonami = {}
+
+-- Mobs
+dofile(minetest.get_modpath("zoonami") .. "lua/mobs.lua")
+
+-- Battle
+dofile(minetest.get_modpath("zoonami") .. "lua/battle.lua")
+
+-- SFINV
+dofile(minetest.get_modpath("zoonami") .. "lua/sfinv.lua")
