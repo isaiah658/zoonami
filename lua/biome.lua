2f2d21c60e860a83d22ef28d7dcb8df9ca152502
isaiah658 <29903689+isaiah658@users.noreply.github.com>
2020-10-01 00:00:49 +0000

    Update biome.lua
    Improves the detection method for water.

--- lua/biome.lua
+++ lua/biome.lua
@@ -36,30 +36,31 @@ biome.underground = "underground"
 
 function biome.background(mt_player_obj)
 	local player_pos = mt_player_obj:get_pos()
-	player_pos.y = player_pos.y - 0.5
 	local biome_data = minetest.get_biome_data(player_pos)
 	local biome_name = minetest.get_biome_name(biome_data.biome)
 	if biome[biome_name] then
 		return "zoonami_"..biome[biome_name].."_background.png"
 	else
-		local node = minetest.get_node(player_pos)
-		if string.find(node.name, "grass") and biome_data.humidty and biome_data.humidty < 70 then
-			return "zoonami_grassland_background.png"
-		elseif string.find(node.name, "grass") and biome_data.humidty and biome_data.humidty >= 70 then
-			return "zoonami_forest_background.png"
-		elseif string.find(node.name, "grass") then
-			return "zoonami_grassland_background.png"
-		elseif string.find(node.name, "snow") then
-			return "zoonami_snowy_background.png"
-		elseif string.find(node.name, "sand") and biome_data.humidty and biome_data.humidty < 40 then
-			return "zoonami_desert_background.png"
-		elseif string.find(node.name, "sand") and biome_data.humidty and biome_data.humidty > 40 then
-			return "zoonami_beach_background.png"
-		elseif string.find(node.name, "sand") then
-			return "zoonami_desert_background.png"
-		elseif string.find(node.name, "water") then
+		local node_in_player = minetest.get_node(player_pos)
+		player_pos.y = player_pos.y - 0.5
+		local node_under_player = minetest.get_node(player_pos)
+		if string.find(node_in_player.name, "water") or string.find(node_under_player.name, "water") then
 			return "zoonami_underwater_background.png"
-		elseif string.find(node.name, "stone") and player_pos.y <= -10 then
+		elseif string.find(node_under_player.name, "grass") and biome_data.humidty and biome_data.humidty < 70 then
+			return "zoonami_grassland_background.png"
+		elseif string.find(node_under_player.name, "grass") and biome_data.humidty and biome_data.humidty >= 70 then
+			return "zoonami_forest_background.png"
+		elseif string.find(node_under_player.name, "grass") then
+			return "zoonami_grassland_background.png"
+		elseif string.find(node_under_player.name, "snow") then
+			return "zoonami_snowy_background.png"
+		elseif string.find(node_under_player.name, "sand") and biome_data.humidty and biome_data.humidty < 40 then
+			return "zoonami_desert_background.png"
+		elseif string.find(node_under_player.name, "sand") and biome_data.humidty and biome_data.humidty > 40 then
+			return "zoonami_beach_background.png"
+		elseif string.find(node_under_player.name, "sand") then
+			return "zoonami_desert_background.png"
+		elseif string.find(node_under_player.name, "stone") and player_pos.y <= -10 then
 			return "zoonami_underground_background.png"
 		else
 			return "zoonami_grassland_background.png"
