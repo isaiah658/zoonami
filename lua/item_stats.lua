aecc5e0be6f3da725575fc7eafdbf9db3cf7a3d9
isaiah658 <29903689+isaiah658@users.noreply.github.com>
2020-10-21 01:22:31 +0000

    Add files via upload

---
+++ lua/item_stats.lua
@@ -0,0 +1,23 @@
+-- The item stats for items used in battles
+
+local item_stats = {}
+
+item_stats.red_berry = {
+	name = "Red Berry",
+	asset_name = "red_berry",
+	description = "+10 Health",
+	type = "healing",
+	amount = 10,
+	color = "#FF3321FF"
+}
+
+item_stats.blue_berry = {
+	name = "Blue Berry",
+	asset_name = "blue_berry",
+	description = "+10 Health",
+	type = "healing",
+	amount = 10,
+	color = "#2F7DB5FF"
+}
+
+return item_stats
